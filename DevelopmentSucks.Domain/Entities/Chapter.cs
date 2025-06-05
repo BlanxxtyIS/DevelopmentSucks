c45e15049b5941dae71f83041d7d6e64315cb423
MarlineIOS <blanxxtygadress@gmail.com>
2025-06-05 14:09:22 +0300

    Initial create\

---
+++ DevelopmentSucks.Domain/Entities/Chapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopmentSucks.Domain.Entities;
+
+public class Chapter
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public int Order { get; set; }
+    public Guid CourseId { get; set; }
+    public Course Course { get; set; }
+
+    public List<Lesson> Lessons { get; set; } = new();
+}
---
+++ DevelopmentSucks.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DevelopmentSucks.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopmentSucks.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Chapter> Chapters { get; set; }
+    public DbSet<Lesson> Lessions { get; set; }
+}
