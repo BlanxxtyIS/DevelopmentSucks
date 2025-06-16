ed052f6916927cc75627097675689274a15ae203
MarlineIOS <blanxxtygadress@gmail.com>
2025-06-16 18:45:27 +0300

    added light valid

--- DevelopmentSucks.Application/Contracts/DTO/ChapterDto.cs
+++ DevelopmentSucks.Application/Contracts/DTO/ChapterDto.cs
@@ -1,6 +1,7 @@
 ﻿using DevelopmentSucks.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace DevelopmentSucks.Application.Contracts.DTO;
 
 public class ChapterDto {
     public Guid? Id { get; set; }
+
+    [Required(ErrorMessage = "Название главы обязательо")]
+    [MaxLength(20, ErrorMessage = "Максимальная длина главы курса 20 символов")]
     public string Title { get; set; } = string.Empty;
     public int Order { get; set; }
     public Guid CourseId { get; set; }
