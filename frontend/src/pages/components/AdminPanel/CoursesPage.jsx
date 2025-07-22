1b0288974c4dbfbe9bd99e23e86ac41304fb423f
MarlineIOS <blanxxtygadress@gmail.com>
2025-07-22 15:21:42 +0300

    fixed ci/cd

--- frontend/src/pages/components/AdminPanel/CoursesPage.jsx
+++ frontend/src/pages/components/AdminPanel/CoursesPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback} from "react";
 import * as coursesApi from '../../../api/coursesApi'
 
 export default function CoursesPage() {
